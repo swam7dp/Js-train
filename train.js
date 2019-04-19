a659a56675456470df492d84442943f0fd8df388
Evgenyi <swam7dp@gmail.com>
2019-04-19 12:08:39 +0300

    Это наш первый комит

---
+++ train.js
@@ -0,0 +1,79 @@
+//'use strict';
+a = 5;
+console.log(a);
+var number1 = 2;
+var number2 = 6;
+var sum;
+sum = number1 + number2;
+console.log(sum);
+console.log(number1);
+console.log(number2);
+//однострочный коментарий
+/* многострочный 
+коментарий*/
+
+var leftBorderWidth = 1;
+{
+let second =2;
+console.log(second);
+}
+const pi =3.14;
+
+console.log(4/0);
+console.log('string'*9);
+
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+
+console.log(person["name"]);
+
+let arr = ['plum.png','orang.jpeg','apple.bmp'];
+console.log(arr[1]);
+//alert("hello world");
+
+//let answer = confirm("are you here?");
+//console.log(answer);
+
+//let answer = +prompt("Est li vam 18?", "Da");
+//console.log(answer);
+
+//console.log(4+ " - object");
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+console.log(incr);
+console.log(decr);
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
+
+
+
+
+
+
+
+
+
+
+
+
+
